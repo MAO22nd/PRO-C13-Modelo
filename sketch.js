938a931aec11aeb6faa55f9955f68dc5f03ffa42
MAO22nd <129629549+MAO22nd@users.noreply.github.com>
2023-05-03 22:09:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,9 +12,9 @@ function preload(){
   blue_balloonImage = loadImage("blue_balloon0.png");
 
   // red_balloonImage = loadImage("red_balloon0.png");
-  // red_balloonImage = loadImage("redballoon0.png");
+//  red_balloonImage = loadImage("redballoon0.png");
   // red_balloonImage = loadImage("red_balloon0");
-  //red_balloonImage = loadImage("red_balloon0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
 
 }
 
@@ -56,9 +56,9 @@ function draw() {
   
   //criando inimigos continuamente
  
-   // var select_balloon = Math.round(random(1,4));
-  // var select_balloon = random(1,4);
-  // var select_balloon = Math.round(random());
+    var select_balloon = Math.round(random(1,4));
+   var select_balloon = random(1,4);
+   var select_balloon = Math.round(random());
   // var select_balloon = Math.round(random(1,4,2));
   
   if (World.frameCount % 100 == 0) {
@@ -97,13 +97,13 @@ function draw() {
 //   red.scale = 0.1;
 // }
 
-// function redBalloon() {
-//   var red = createSprite(,Math.round(random(20, 370)),50, 10, 10);
-//   red.addImage(red_balloonImage);
-//   red.velocityX = 3;
-//   red.lifetime = 150;
-//   red.scale = 0.1;
-// }
+ function redBalloon() {
+   var red = createSprite(,Math.round(random(20, 370)),50, 10, 10);
+   red.addImage(red_balloonImage);
+   red.velocityX = 3;
+  red.lifetime = 150;
+   red.scale = 0.1;
+ }
 
 // function redBalloon() {
 //   var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
